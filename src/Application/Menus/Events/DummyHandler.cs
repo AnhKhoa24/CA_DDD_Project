2df93cef69f198a7b777e4334246e91b8db2fe74
Anh Khoa <anhkhoa.24052003@gmail.com>
2025-07-11 11:12:13 +0700

    rebuild project🧹🧹

--- src/Application/Menus/Events/DummyHandler.cs
+++ src/Application/Menus/Events/DummyHandler.cs
@@ -7,6 +7,7 @@ public class DummyHandler : INotificationHandler<MenuCreated>
 {
    public Task Handle(MenuCreated notification, CancellationToken cancellationToken)
    {
+      // throw new Exception("");
       return Task.CompletedTask;
    }
 }
